42b1d2aaedd8fcd1b53df0f4195e60a190dd0963
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2019-11-09 11:06:37 +0100

    Add artifact version to response

--- src/main/java/uk/co/automatictester/simple/app/SimpleWebServer.java
+++ src/main/java/uk/co/automatictester/simple/app/SimpleWebServer.java
@@ -6,14 +6,25 @@ import java.util.Random;
 
 public class SimpleWebServer {
 
+    private static final int RUNTIME_ID = new Random().nextInt(8999) + 1000;
+    private static Random rand = new Random();
+
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(8080);
         app.get("/", ctx -> ctx.result(getRandomNumber()));
     }
 
     private static String getRandomNumber() {
-        Random rand = new Random();
-        int randomNumber = rand.nextInt(8999) + 1000;
-        return String.format("haha 4: %d", randomNumber);
+        int requestId = rand.nextInt(8999) + 1000;
+        return String.format("Version:   %s\nRuntimeId: %d\nRequestId: %d",
+                appVersion(),
+                RUNTIME_ID,
+                requestId
+        );
+    }
+
+    private static String appVersion() {
+        String version = SimpleWebServer.class.getPackage().getImplementationVersion();
+        return version == null ? "unknown" : version;
     }
 }
